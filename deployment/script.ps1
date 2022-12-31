e240fd49b3faa14425c53977abbcf2404187b72d
alex-toy <alexei.80@hotmail.fr>
2022-12-31 12:57:13 +0100

    update templates

---
+++ deployment/script.ps1
@@ -0,0 +1,27 @@
+az group create `
+    --location francecentral `
+    --name alexeirg
+
+
+# az deployment group create `
+#     --resource-group alexeirg `
+#     --template-file 'deployment\database\template.json' `
+#     --parameters 'deployment\database\parameters.json'
+
+
+# az deployment group create `
+#     --resource-group alexeirg `
+#     --template-file 'deployment\factory\template.json' `
+#     --parameters 'deployment\factory\parameters.json'
+
+
+# az deployment group create `
+# --resource-group alexeirg `
+# --template-file 'deployment\storage\template.json' `
+# --parameters 'deployment\storage\parameters.json'
+
+
+az deployment group create `
+    --resource-group alexeirg `
+    --template-file 'deployment\gen2\template.json' `
+    --parameters 'deployment\gen2\parameters.json'
